92f091a51b0b7627ccafa934ec92576a2af1c994
nataberger <natasha.berger@innew.la>
2024-12-28 21:39:59 -0300

    fix date

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,13 +1,23 @@
 import { useState, useContext } from 'react';
 import './Modal.scss'
 
-const Modal = () => {
-  return (
-    <div className='modal--backdrop'>
-      <div className='modal--container'>
-        <p>Modal</p>
-      </div>
+const Modal = ({children}) => {
+  // const [open, setOpen ] = useState(false)
 
+  // const handleOpen = () => {
+  //   setOpen(true)
+  // }
+
+  return (
+    <div 
+    className={`${!open ?  '.hidden' : ''}`} 
+    >
+      <div className='modal--backdrop'>
+        <div className='modal--container'>
+          {children}
+        </div>
+
+      </div>
     </div>
   )
 }
