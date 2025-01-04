714a1915db9d907693f446f52b421e05d4322a2a
nataberger <natasha.berger@innew.la>
2025-01-03 21:54:53 -0300

    fix gear icon

--- src/components/toggle/Toggle.jsx
+++ src/components/toggle/Toggle.jsx
@@ -1,16 +1,20 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
 
 
 export const Toggle = ({children, isHidden, setState}) => {
-
+  const [iconstyle, setIconstyle] = useState("inverted");
   const handleClick = () => {
     setState(!isHidden)
 
   }
+  const removeInverse = () => {
+    setIconstyle(!iconstyle)
+  }
   return (
     <>
-     <button className='config' onClick={handleClick}>
-        <FontAwesomeIcon icon={['fas', 'gear']} inverse />
+     <button className='config' onClick={handleClick} onMouseEnter={removeInverse}>
+        <FontAwesomeIcon icon={['fas', 'gear']} iconstyle={setIconstyle}/>
       </button>
       {
         !isHidden && children
